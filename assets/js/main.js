77097ee35f8012739e58b9e984c64d343c897dcb
Drew <andrewsiruma02@gmail.com>
2024-05-20 22:43:59 +0800

    This is the initial commit for front-end of the website

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    headerEl.classList.add('header-scrolled');
+  } else if (window.scrollY <= 50) {
+    headerEl.classList.remove('header-scrolled');
+  }
+});
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select('.header')
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop
+    let nextElement = selectHeader.nextElementSibling
+    const headerFixed = () => {
+      if ((headerOffset - window.scrollY) <= 0) {
+        selectHeader.classList.add('fixed-top')
+        nextElement.classList.add('scrolled-offset')
+      } else {
+        selectHeader.classList.remove('fixed-top')
+        nextElement.classList.remove('scrolled-offset')
+      }
+    }
+    window.addEventListener('load', headerFixed)
+    onscroll(document, headerFixed)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+
+})()
---
+++ pages/index.php
@@ -0,0 +1,115 @@
+<! DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+        
+        <!-- VENDOR CSS -->
+        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="../assets/vendor/fontawesome/css/all.min.css" rel="stylesheet">
+        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        <!-- MAIN CSS -->
+        <link href="../assets/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <!-- ======= Top Bar ======= -->
+        <section id="topbar" class="d-flex align-items-center">
+            <div class="container d-flex justify-content-center justify-content-md-between">
+                <div class="contact-info d-flex align-items-center">
+                    <i class="bi bi-hourglass-split"></i> Office Hours&nbsp;<b>Mon - Fri: 9:00 am - 6:00 pm</b>
+                </div>
+                <div class="social-links d-none d-md-block">
+                    
+                    <a href="mailto:pdc_email@sample.com"><i class="bi bi-envelope-fill"></i></a>
+                    <a href="https://www.facebook.com/" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+                    
+                </div>
+            </div>
+        </section>
+        <!-- ======= End Top Bar ======= -->
+
+        <!-- ======= Navigation ======= -->
+        <header class="header">
+            <div class="container d-flex align-items-center">
+                <a href="index.php" class="logo me-auto"><img src="../assets/img/PDC-Logo.png" alt="logo" class="img-fluid"></a>
+                
+                <nav id="navbar" class="navbar"><ul>
+                    <li><a class="nav-link active" href="#">Home</a></li>
+                    <li><a class="nav-link" href="#">About</a></li>
+                    <li><a class="nav-link" href="#">Careers</a></li>
+                    <li><a class="nav-link" href="#">Promos</a></li>
+                    <li><a class="nav-link" href="#">Contact Us</a></li>
+                </ul><i class="bi bi-list mobile-nav-toggle"></i></nav>
+            </div>
+        </header>
+        <!-- ======= End Navigation ======= -->
+
+        <!-- ======= Hero ======= -->
+        <section id="hero">
+            <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+                <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+                <div class="carousel-inner" role="listbox">
+
+                    <!-- Slide 1 -->
+                    <div class="carousel-item active" style="background-image: url(../assets/img/slides/slide-1.jpg)">
+                    </div>
+
+                    <!-- Slide 2 -->
+                    <div class="carousel-item" style="background-image: url(../assets/img/slides/slide-2.jpg)">
+                    </div>
+
+                    <!-- Slide 3 -->
+                    <div class="carousel-item" style="background-image: url(../assets/img/slides/slide-3.jpg)">
+                        <div class="carousel-container">
+                            <div class="container">
+                            <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
+                            <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+                </a>
+
+                <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+                </a>
+
+            </div>
+        </section>
+        <!-- ======= End Hero ======= -->
+
+        <!-- ======= Cta Section ======= -->
+        <section id="cta" class="cta">
+            <div class="container">
+
+                <div class="row">
+                <div class="col-lg-9 text-center text-lg-start">
+                    <h3>Who are we?</h3>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-lg-3 cta-btn-container text-center">
+                    <a href="#" class="cta-btn align-middle">Join Now</a>
+                </div>
+                </div>
+
+            </div>
+        </section>
+        <!-- ======= End Cta Section ======= -->
+
+    </body>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <!-- Template Main JS File -->
+    <script src="../assets/js/main.js"></script>
+</html>
