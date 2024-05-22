980c9d547231f2116748e265b69bcb267e17cb93
Drew <andrewsiruma02@gmail.com>
2024-05-22 10:48:51 +0800

    Added links on Navbar

--- pages/index.php
+++ pages/index.php
@@ -41,11 +41,11 @@
                 <a href="index.php" class="logo me-auto"><img src="../assets/img/PDC-Logo.png" alt="logo" class="img-fluid"></a>
                 
                 <nav id="navbar" class="navbar"><ul>
-                    <li><a class="nav-link active" href="#">Home</a></li>
-                    <li><a class="nav-link" href="#">About</a></li>
-                    <li><a class="nav-link" href="#">Careers</a></li>
-                    <li><a class="nav-link" href="#">Promos</a></li>
-                    <li><a class="nav-link" href="#">Contact Us</a></li>
+                    <li><a class="nav-link active" href="index.php">Home</a></li>
+                    <li><a class="nav-link" href="about.php">About</a></li>
+                    <li><a class="nav-link" href="careers.php">Careers</a></li>
+                    <li><a class="nav-link" href="promos.php">Promos</a></li>
+                    <li><a class="nav-link" href="contact.php">Contact Us</a></li>
                 </ul><i class="bi bi-list mobile-nav-toggle"></i></nav>
             </div>
         </header>
@@ -217,96 +217,94 @@
 
         <!-- ======= Footer Section ======= -->
         <footer class="text-center text-lg-start text-white" style="background: #000;">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-      <!-- Section: Links -->
-      <section class="">
-        <!--Grid row-->
-        <div class="row">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-            <div class="text-center">
-                <img src="../assets/img/PDC-Logo.png" style="max-width:50%;vertical-align: middle;margin-bottom:30px;" class="img-fluid" alt="logo">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+                <!-- Section: Links -->
+                <section class="">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <div class="text-center">
+                                <img src="../assets/img/PDC-Logo.png" style="max-width:50%;vertical-align: middle;margin-bottom:30px;" class="img-fluid" alt="logo">
+                            </div>
+                            <p>This is a short sentence</p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                            <p><i class="fas fa-home mr-3"></i> BLDG 2A PHILCREST I CPD. KM 23 West Service Rd, Cupang, Muntinlupa City, NCR</p>
+                            <p><i class="fas fa-envelope mr-3"></i> pandadevtcorp@yahoo.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> +63 (2) 8 846 2961</p>
+                            <p><i class="fas fa-phone mr-3"></i> +63 (2) 8 846 1806</p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Outlets</h6>
+                            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Las Piñas</p>
+                            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Muntinlupa</p>
+                            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Parañaque</p>
+                            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Quezon</p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15455.610229319407!2d121.0441929!3d14.4327788!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d14e5eda34cb%3A0xc16f7f75511dfee!2sPanda%20Development%20Corp!5e0!3m2!1sen!2sph!4v1716343190339!5m2!1sen!2sph" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!--Grid row-->
+                </section>
+                <!-- Section: Links -->
+
+                <hr class="my-3">
+
+                <!-- Section: Copyright -->
+                <section class="p-3 pt-0">
+                    <div class="row d-flex align-items-center">
+                        <!-- Grid column -->
+                        <div class="col-md-7 col-lg-8 text-center text-md-start">
+                            <!-- Copyright -->
+                            <div class="p-3">
+                                © 2024 Copyright: Panda Development Corporation.
+                            </div>
+                            <!-- Copyright -->
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                            <!-- Facebook -->
+                            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                            <!-- Twitter -->
+                            <a class="btn btn-outline-light btn-floating m-1ctext-white" role="button"><i class="fab fa-twitter"></i></a>
+
+                            <!-- Google -->
+                            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-google"></i></a>
+
+                            <!-- Instagram -->
+                            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-instagram"></i></a>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                </section>
+                <!-- Section: Copyright -->
             </div>
-            <p>This is a short sentence</p>
-          </div>
-          <!-- Grid column -->
-
-          <hr class="w-100 clearfix d-md-none" />
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-            <p><i class="fas fa-home mr-3"></i> BLDG 2A PHILCREST I CPD. KM 23 West Service Rd, Cupang, Muntinlupa City, NCR</p>
-            <p><i class="fas fa-envelope mr-3"></i> pandadevtcorp@yahoo.com</p>
-            <p><i class="fas fa-phone mr-3"></i> +63 (2) 8 846 2961</p>
-            <p><i class="fas fa-phone mr-3"></i> +63 (2) 8 846 1806</p>
-          </div>
-
-          <!-- Grid column -->
-
-          <hr class="w-100 clearfix d-md-none" />
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Outlets</h6>
-            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Las Piñas</p>
-            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Muntinlupa</p>
-            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Parañaque</p>
-            <p><i class="fas fa-map-marker-alt"></i> Dunkin' - Quezon</p>
-          </div>
-          
-
-          <!-- Grid column -->
-          <hr class="w-100 clearfix d-md-none" />
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15455.610229319407!2d121.0441929!3d14.4327788!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d14e5eda34cb%3A0xc16f7f75511dfee!2sPanda%20Development%20Corp!5e0!3m2!1sen!2sph!4v1716343190339!5m2!1sen!2sph" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-          </div>
-          <!-- Grid column -->
-        </div>
-        <!--Grid row-->
-      </section>
-      <!-- Section: Links -->
-
-      <hr class="my-3">
-
-      <!-- Section: Copyright -->
-      <section class="p-3 pt-0">
-        <div class="row d-flex align-items-center">
-          <!-- Grid column -->
-          <div class="col-md-7 col-lg-8 text-center text-md-start">
-            <!-- Copyright -->
-            <div class="p-3">
-              © 2024 Copyright: Panda Development Corporation.
-            </div>
-            <!-- Copyright -->
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-            <!-- Facebook -->
-            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-facebook-f"></i></a>
-
-            <!-- Twitter -->
-            <a class="btn btn-outline-light btn-floating m-1ctext-white" role="button"><i class="fab fa-twitter"></i></a>
-
-            <!-- Google -->
-            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-google"></i></a>
-
-            <!-- Instagram -->
-            <a class="btn btn-outline-light btn-floating m-1 text-white" role="button"><i class="fab fa-instagram"></i></a>
-          </div>
-          <!-- Grid column -->
-        </div>
-      </section>
-      <!-- Section: Copyright -->
-    </div>
-    <!-- Grid container -->
-  </footer>
-  <!-- ======= End Footer Section ======= -->
+            <!-- Grid container -->
+        </footer>
+        <!-- ======= End Footer Section ======= -->
 
     </body>
     <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
