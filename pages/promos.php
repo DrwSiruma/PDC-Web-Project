8de06d926a89be7b8b9d0847afcaf9227b098266
Drew <andrewsiruma02@gmail.com>
2024-05-24 18:02:43 +0800

    added local promo

--- pages/promos.php
+++ pages/promos.php
@@ -1,3 +1,76 @@
 <?php include('header.php'); ?>
-    <!-- Place the content here. -->
+    <!-- ======= Hero ======= -->
+    <section id="hero" class="hero">
+        <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+            <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+            <div class="carousel-inner" role="listbox">
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active" style="background-image: url(../assets/img/slides/slide-1.jpg)">
+                </div>
+
+                <!-- Slide 2 -->
+                <div class="carousel-item" style="background-image: url(../assets/img/slides/slide-2.jpg)">
+                </div>
+
+                <!-- Slide 3 -->
+                <div class="carousel-item" style="background-image: url(../assets/img/slides/slide-3.jpg)">
+                </div>
+
+            </div>
+
+            <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            </a>
+
+            <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            </a>
+
+        </div>
+    </section>
+    <!-- ======= End Hero ======= -->
+
+    <section id="promo" class="promo">
+        <div class="container">
+            <div class="section-title">
+                <h2>Promos</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="promo-card">
+                        <img src="../assets/img/promo/BEARY IN LOVE.png" alt="">
+                        <h4>Local Promo 1</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <!-- <button class="btn"></button> -->
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="promo-card">
+                        <img src="../assets/img/promo/EASTER MUNCHKIN DEAL.png" alt="">
+                        <h4>Local Promo 2</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="promo-card">
+                        <img src="../assets/img/promo/SUMMER DELIGHT PROMO.png" alt="">
+                        <h4>Local Promo 3</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php include('footer.php'); ?>
