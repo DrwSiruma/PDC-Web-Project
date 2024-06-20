9ea01ccd669708b67f645db47d26e4aebe1db5c2
Drew <andrewsiruma02@gmail.com>
2024-06-20 09:27:00 +0800

    Marketing Feedback Page Ready

--- pages/marketing/marketing.feedback.php
+++ pages/marketing/marketing.feedback.php
@@ -1,5 +1,8 @@
 <?php include('marketing.header.php'); ?>
 
-    <!-- CONTENT -->
+    <div id="content" class="p-4 p-md-5 pt-5">
+        <h2 class="mb-4 text-orange">Feedback&nbsp;<a href="#" class="btn btn-orange"><i class="fas fa-plus"></i></a></h2>
+        <hr />
+    </div>
 
 <?php include('marketing.footer.php'); ?>
