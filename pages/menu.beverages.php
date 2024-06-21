517533cecf3955fffd030eb1405c252a520cdc86
Drew <andrewsiruma02@gmail.com>
2024-06-21 16:30:27 +0800

    Set indicators for outlet menus

--- pages/menu.beverages.php
+++ pages/menu.beverages.php
@@ -19,17 +19,30 @@ $id = $_GET['id'];
                 <?php
                     $product_qry = mysqli_query($conn, "SELECT tom.id AS outlet_menu_id, tom.product_id, tom.outlet_id, tbo.id AS outlet_id, tbo.store_name, tp.id AS product_id, tp.name AS product_name, tp.category AS product_category, tp.image_name FROM tbl_outlet tbo LEFT JOIN tbl_outlet_menu tom ON tbo.id = tom.outlet_id LEFT JOIN tbl_product tp ON tom.product_id = tp.id WHERE tp.category = 'Beverage' AND tom.outlet_id = $id;");
 
-                    while($product_row = mysqli_fetch_array($product_qry)) {
+                    if (mysqli_num_rows($product_qry) > 0) {
+
+                        while($product_row = mysqli_fetch_array($product_qry)) {
                 ?>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="product-card">
-                            <div class="product-image">
-                                <img src="../uploads/product/<?php echo $product_row['image_name']; ?>" alt="<?php echo htmlspecialchars($product_row['product_name']); ?>" class="img-fluid">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="product-card">
+                                <div class="product-image">
+                                    <img src="../uploads/product/<?php echo $product_row['image_name']; ?>" alt="<?php echo htmlspecialchars($product_row['product_name']); ?>" class="img-fluid">
+                                </div>
+                                <div class="product-name"><?php echo htmlspecialchars($product_row['product_name']); ?></div>
                             </div>
-                            <div class="product-name"><?php echo htmlspecialchars($product_row['product_name']); ?></div>
                         </div>
-                    </div>
-                <?php } ?>
+                <?php
+                        }
+                    } else {
+                ?>
+                        <div class="col-12">
+                            <div class="alert alert-warning text-center" role="alert">
+                                No available products in this category.
+                            </div>
+                        </div>
+                <?php 
+                    }
+                ?>
             </div>
         </div>
     </section>
