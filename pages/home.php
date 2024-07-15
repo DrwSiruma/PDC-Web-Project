b6f9b32c3a2818434cef03c6cabf923917da4328
Drew <andrewsiruma02@gmail.com>
2024-07-15 16:37:23 +0800

    commit

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,33 @@
 <?php include('header.php'); ?>
 
+    <section id="banner" style="background: #F9F3EC;">
+        <div class="container">
+            <div class="swiper main-swiper">
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide py-5">
+                        <div class="row banner-content align-items-center">
+                            <div class="img-wrapper col-md-5">
+                                <img src="../assets/img/menu/delights_bundle.png" class="img-fluid">
+                            </div>
+                            <div class="content-wrapper col-md-7 p-5 mb-5">
+                                <div class="secondary-font text-primary text-uppercase mb-4">Save 10 - 20 % off</div>
+                                <h2 class="banner-title display-1 fw-normal">Best destination for <span class="text-primary">your pets</span></h2>
+                                <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">
+                                    shop now
+                                    <svg width="24" height="24" viewBox="0 0 24 24" class="mb-1">
+                                        <use xlink:href="#arrow-right"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!-- ======= Hero ======= -->
     <section id="hero" class="hero">
         <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
