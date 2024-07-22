d37a1abda3e8480b2d99c1d23bfa406aac9ec014
Drew <andrewsiruma02@gmail.com>
2024-07-22 10:48:06 +0800

    commit

--- pages/home.php
+++ pages/home.php
@@ -19,17 +19,10 @@
                     <div class="swiper-slide py-5">
                         <div class="row banner-content align-items-center">
                             <div class="img-wrapper col-md-5">
-                                <img src="../assets/img/menu/delights_bundle.png" class="img-fluid">
+                                <img src="../assets/img/menu/munchkins_bucket.png" class="img-fluid">
                             </div>
                             <div class="content-wrapper col-md-7 p-5 mb-5">
-                                <div class="secondary-font text-primary text-uppercase mb-4">Save 10 - 20 % off</div>
-                                <h2 class="banner-title display-1 fw-normal">Best destination for <span class="text-primary">your pets</span></h2>
-                                <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">
-                                    shop now
-                                    <svg width="24" height="24" viewBox="0 0 24 24" class="mb-1">
-                                        <use xlink:href="#arrow-right"></use>
-                                    </svg>
-                                </a>
+                                <h2 class="banner-title display-1 fw-normal">Munchkin Mania: Grab Your Bucket of Joy!</h2>
                             </div>
                         </div>
                     </div>
